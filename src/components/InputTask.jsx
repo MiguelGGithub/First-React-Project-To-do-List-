b571c913a4a5c0c8c7365cd6453582155ea04fa4
Miguel Gomez <miguelgomezca2021@gmail.com>
2023-07-20 12:47:13 -0400

    Wrong format

--- src/components/InputTask.jsx
+++ src/components/InputTask.jsx
@@ -97,7 +97,7 @@ export function InputTask(props) {
         </div>
         {!isTimeInputValid && (
           <div ref={warningRef} className="time-input-warning shake">
-            <h1>Please enter a valid task or time (e.g., `Friday 9PM` or `Today 1:30 PM`).</h1>
+            Please enter a valid task or time (e.g., 'Friday 9PM' or 'Today 1:30 PM')
           </div>
         )}
         <button type="submit">Add Task</button>
